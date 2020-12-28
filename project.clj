99975adf20ece1848398cf935102d65f46006f02
Maximilian Karasz <mknoszlig@zeroton.in>
2020-12-28 15:49:45 +0100

    add url and description.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject dataflocks/clj-logback-event-evaluator "0.1.0"
-  :description "FIXME: write description"
+  :description "a logback event evaluator that evaluates clojure expressions"
   :license {:name "MIT License"
             :url  "https://opensource.org/licenses/MIT"}
   :aot :all
+  :url "https://github.com/dataflocks/clj-logback-event-evaluator"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ch.qos.logback/logback-classic "1.2.3"]]
   :repl-options {:init-ns dataflocks.clj-logback-event-evaluator})
